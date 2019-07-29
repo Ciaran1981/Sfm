2ae3b9f8a24fd1e41ab5a54c41b379fd1d00b32b
Ciaran1981 <ciaran.robb@gmail.com>
2019-07-29 11:22:04 +0100

    mosaic fixes and additions

--- substages/SeamMosaic.py
+++ substages/SeamMosaic.py
@@ -10,7 +10,7 @@ SeamMosaic.py -folder PIMs-Ortho -algo Fraser
 
 Use the micmac teslib seamline mosaicing function - which requires a labourious
 
-image pattern so have used python here 
+image pattern so have used python here as typing is so strenuous
 
 
 """
@@ -63,4 +63,113 @@ mm3d = ["mm3d", "TestLib", "SeamlineFeathering", '"'+sub2+'"',  "Out=SeamMosaic.
 
 #"ApplyRE=1"
 call(mm3d)
+"""
+# TODO
+# Here follows the approx code for the py implementation 
+pairs = [("orthopath1", "orthopath2"), ("orthopath1", "orthopath2")]
+
+# Observations used to adjust the overall model of the pair
+obsGlob = ?? (i,j) # this seems to imply a pixel coord or block
+
+# Observations used to calculate a mean image equalization model
+obsMn = ??
+
+# Must open stuff and set the block size
+
+inDataset = gdal.Open(inputIm)
+
+    
+outDataset = _copy_dataset_config(inputIm, outMap = outputIm,
+                             bands = inDataset.RasterCount)
+bnd = inDataset.GetRasterBand(1)
+
+outBand = outDataset.GetRasterBand(1)
+cols = inDataset.RasterXSize
+rows = inDataset.RasterYSize
+# So with most datasets blocksize is a row scanline
+if blocksize == None:
+    blocksize = bnd.GetBlockSize()
+    blocksizeX = blocksize[0]
+    blocksizeY = blocksize[1]
+else:
+    blocksizeX = blocksize
+    blocksizeY = blocksize
+
+# loop
+
+for p in pairs:
+  # use the usual gdal routine here range (i, j)
+    
+    for i in tqdm(range(0, rows, blocksizeY)):
+            if i + blocksizeY < rows:
+                numRows = blocksizeY
+            else:
+                numRows = rows -i
+        
+            for j in range(0, cols, blocksizeX):
+                if j + blocksizeX < cols:
+                    numCols = blocksizeX
+                else:
+                    numCols = cols - j
+
+               
+                array = bnd.ReadAsArray(j, i, numCols, numRows)
+                
+                # pseudo of what is to be done
+                #RadiomTuileImagei=a+b∗RadiomTuileImagj
+                #Obs1tuile= (a+b∗Quartile1(RadiomTuileImagej), Quartile1(RadiomTuileImagej)
+                #Obs2tuile= (a+b∗Quartile3(RadiomTuileImagej), Quartile3(RadiomTuileImagej)
+                
+                #AjoutObs1tuileàObsGlobi,i
+                #AjoutObs2tuileàObsGlobi,j
+                # this loop ends
+            RadiomImagei=a+b∗RadiomImage
+            Obs1ImCpl= (a+b∗Quartile1(RadiomImagej), Quartile1(RadiomImagej)
+            Obs2ImCpl= (a+b∗Quartile3(RadiomImagej), Quartile3(RadiomImagej)
+            AjoutObs1ImCplàObsGlobImii
+            AjoutObs1ImCplàObsGlobImii
+                #outBand.WriteArray(array, j, i) 
+        
+""" 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- substages/orthomosaic.sh
+++ substages/orthomosaic.sh
@@ -18,7 +18,9 @@ while getopts "f:u:mt:o:h" opt; do
       echo "Run the workflow for drone acquisition at nadir (and pseudo nadir) angles)."
       echo "orthomosaic.sh -f $PWD -u '30 +north' -mt ossimFeatherMosaic -o outmosaic.tif"
       echo "	-f FOLDER     : MicMac working directory."
-      echo "	-u UTMZONE       : UTM Zone of area of interest. Takes form 'NN +north(south)'"
+      echo "	-u UTMZONE       : UTM Zone of area of interest"
+      echo "    -mb MBATCH : whether to us maltbatch (bool)"
+      echo "    -pb PBATCH : whether to us pimsbatch (bool)"      
       echo " -mt MTYPE        : OSSIM mosaicing type e.g. ossimBlendMosaic ossimMaxMosaic ossimImageMosaic ossimClosestToCenterCombiner ossimBandMergeSource ossimFeatherMosaic" 
       echo "	-o OUT       : Output mosaic e.g. mosaic.tif"      
       echo "	-h	             : displays this message and exits."
@@ -33,6 +35,12 @@ while getopts "f:u:mt:o:h" opt; do
       UTM=$OPTARG
       utm_set=true
       ;;
+	mb)
+      MBATCH=$OPTARG
+      ;;   
+	pb)
+      PBATCH=$OPTARG
+      ;;   
 	mt)
       MTYPE=$OPTARG
       ;;                        
@@ -50,15 +58,53 @@ while getopts "f:u:mt:o:h" opt; do
   esac
 done
 
-echo "geo-reffing  mini ortho-mosaics generated from Malt/Pims/TawnyBatch"
-for f in $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif; do
-    gdal_edit.py -a_srs "+proj=utm +zone=$UTM  +ellps=WGS84 +datum=WGS84 +units=m +no_defs" "$f"; done
+if [ -n "${MBATCH}" ]; then
 
-   
-# this works 
-echo "generating image histograms"
-find $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif | parallel "ossim-create-histo -i {}" 
- 
-# Max seems best
-echo "creating final mosaic"
-ossim-orthoigen --combiner-type $MTYPE  $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif $OUT
+    echo "geo-reffing  mini ortho-mosaics generated from Malt/Pims/TawnyBatch"
+
+    #if [ -n "${tile}" ]; then
+    for f in $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif; do
+        gdal_edit.py -a_srs "+proj=utm +zone=$UTM  +ellps=WGS84 +datum=WGS84 +units=m +no_defs" "$f"; done
+    
+       
+    # this works 
+    echo "generating image histograms"
+    find $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif | parallel "ossim-create-histo -i {}" 
+     
+    # Max seems best
+    echo "creating final mosaic"
+    ossim-orthoigen --combiner-type $MTYPE  $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif $OUT
+fi
+
+if [ -n "${PBATCH}" ]; then
+
+    echo "geo-reffing  mini ortho-mosaics generated from Malt/Pims/TawnyBatch"
+
+    #if [ -n "${tile}" ]; then
+    for f in $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif; do
+        gdal_edit.py -a_srs "+proj=utm +zone=$UTM  +ellps=WGS84 +datum=WGS84 +units=m +no_defs" "$f"; done
+    
+       
+    # this works 
+    echo "generating image histograms"
+    find $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif | parallel "ossim-create-histo -i {}" 
+     
+    # Max seems best
+    echo "creating final mosaic"
+    ossim-orthoigen --combiner-type $MTYPE  $FOLDER/MaltBatch/*tile*/*Ortho-tile*/*Orthophotomosaic.tif $OUT
+else
+    for f in $FOLDER/*Ort_*tif; do
+        mm3d ConvertIm "$f"; done
+        
+    for f in $FOLDER/*Ort_*tif; do
+        gdal_edit.py -a_srs "+proj=utm +zone=$UTM  +ellps=WGS84 +datum=WGS84 +units=m +no_defs" "$f"; done
+    
+       
+    # this works 
+    echo "generating image histograms"
+    find $FOLDER/*Ort_*tif | parallel "ossim-create-histo -i {}" 
+     
+    # Max seems best
+    echo "creating final mosaic"
+    ossim-orthoigen --combiner-type $MTYPE  $FOLDER/*Ort_*tif $OUT
+fi    
